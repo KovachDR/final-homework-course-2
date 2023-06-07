72eb60106d0fdf09ab49a2181a6474ce45ae4565
Brein <Brein_02_93@mail.ru>
2023-06-07 14:59:54 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/demo/FinaltaskApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltaskApplication.class, args);
+	}
+
+}
